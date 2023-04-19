4a8edbcb409ef42ec95eb0e1b90c0a790b907d31
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-04-19 17:46:23 +0200

    adds token generator function

---
+++ src/js/api/requests/create-jwt.js
@@ -0,0 +1,15 @@
+import save from "../../storage/save";
+import { displayErrorMessage } from "../display-error-message";
+
+/**
+ * This function creates a JSON Web Token. It can be used as an replacement to authenticate the current logged in user. The JWT is valid for 15 minute from its creation but will be invalid if user logs out in the time period.
+ */
+export async function createJwt(account) {
+   try {
+      const response = await account.createJWT();
+      const { jwt } = response;
+      save("token", jwt);
+   } catch (error) {
+      displayErrorMessage(error);
+   }
+}
