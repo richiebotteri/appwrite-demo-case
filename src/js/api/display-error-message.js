e657ff4c5fa94ce010811e8d34aaec51ec9a2ea0
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-04-19 14:35:43 +0200

    updating error-msg-el id to class

--- src/js/api/display-error-message.js
+++ src/js/api/display-error-message.js
@@ -1,6 +1,6 @@
 export function displayErrorMessage({ message }) {
    const errorMessage = message;
-   const errorMsgEl = document.querySelector("#error-msg");
+   const errorMsgEl = document.querySelector(".error-msg");
    errorMsgEl.classList.replace("d-none", "d-block");
    errorMsgEl.innerText = message;
 }
