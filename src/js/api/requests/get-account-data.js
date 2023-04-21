a08249f54cd9f1ddd7126d898053372c089e2efa
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-04-21 09:45:05 +0200

    creates function to get active user data

---
+++ src/js/api/requests/get-account-data.js
@@ -0,0 +1,19 @@
+import { Account } from "appwrite";
+import { initiateClient } from "../initiate-client";
+import save from "../../storage/save";
+
+/**
+ * This function gets the account data using the JWT token.
+ * For this to work its required that the JWT token has been create before this one
+ * It also takes a second to save and load the token from localStorage so it wrapped around a setTimeout function.
+ */
+export async function getAccountData(token) {
+   try {
+      const client = initiateClient().setJWT(token);
+      const account = new Account(client);
+      const activeAccountObj = await account.get();
+      save("activeAccountObj", activeAccountObj);
+   } catch (e) {
+      console.log(e);
+   }
+}
