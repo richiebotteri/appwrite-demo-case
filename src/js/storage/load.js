c827640987f27362e6f8ef09f13ce1c4193117b8
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-04-21 10:20:38 +0200

    rm console.log

--- src/js/storage/load.js
+++ src/js/storage/load.js
@@ -2,7 +2,6 @@ export default function (string) {
    const loadItem = localStorage.getItem(string);
    try {
       const objectItem = JSON.parse(loadItem);
-      console.log("A");
       return objectItem;
    } catch (e) {
       return loadItem;
